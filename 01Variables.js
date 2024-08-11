bad797e857495435949663e26068ac1941fea988
Satish Kumar <87963940+satishkr66@users.noreply.github.com>
2024-08-11 16:22:17 +0000

    variable done

---
+++ 01Variables.js
@@ -0,0 +1,15 @@
+const accountId = 144343
+let accontEmail = "satish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gaya"
+
+// accountId = 2
+accontEmail="sk@gmail.com"
+accountPassword="323232323"
+accountCity ="new Delhi"
+
+
+console.log(accountId);
+console.table([accountId,accontEmail,accountPassword,accountCity])
+
+
