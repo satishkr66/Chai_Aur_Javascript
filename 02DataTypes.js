e52afab336051af59c9b7542278bdcd661ba3866
Satish Kumar <87963940+satishkr66@users.noreply.github.com>
2024-08-11 17:34:48 +0000

    Datatype

---
+++ 02DataTypes.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all js code as newer version 
+// alert(3+3)  we are using node js not browser
+console.log(3+3);
+let name ="Satish"
+let age =18
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+
+// object 
+
+    console.log(typeof age);
+    console.log(typeof name);
+    console.log(typeof isloggedin);
+    console.log(typeof undefined); // undefined 
+    console.log(typeof null); //object 
+    
+    
+    
+    
+    
